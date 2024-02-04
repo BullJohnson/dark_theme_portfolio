4fd628f4157f79f1023aedf2a1e906a7474e5be4
bulljohnson <bulljohnson44@gmail.com>
2024-02-04 17:04:11 -0500

    Finish script to add dialog box

--- dark_theme.js
+++ dark_theme.js
@@ -3,19 +3,39 @@
                 // Dark Themed, Minimalist Portfolio | William S. Johnson, Jr
 
 
+let firstName, lastName, company, email, phone, comments;
+
 //functon to gather and display user input for confirmation, before sending                
 function harvest () {
-    $("div[class='form_buttons']").css("display", "none");
+    //$("div[class='form_buttons']").hide();
+//Create a javaScript object from form data
+    let formEntries = new Object();
+    formEntries.firstName = firstName;
+    formEntries.lastName = lastName;
+    formEntries.company = company;
+    formEntries.email = email;
+    formEntries.phone = phone;
+    formEntries.comments = comments;
+//Converting the javaScript Object To a JSON Object
+    const formData = JSON.stringify(formEntries);
+//Parsing the JSON object & creating a heading for dialog box.
+    const boxDisplay = JSON.parse(formData);
+    const heading = "<h4 className=\"ui-state-default ui-corner-all ui-helper-clearfix\" style=\"padding:4px; background: #018f23; text-align: center; margin-top: 0;\">Alien Sighting Report & Json Data<h4>"
+//Displaying the input values, within the dialog box, as a confirmation for the user
+    results.innerHTML = heading + "Name:&emsp;" + boxDisplay.firstName + "&emsp;" + boxDisplay.lastName + 
+        "<br>" + "Company:&emsp;" + boxDisplay.company + "<br>" + "Email:&emsp;" + boxDisplay.email + 
+        "<br>" + "Phone:&emsp;" + boxDisplay.phone + "<br>" + "Comments:&emsp;<br>" + 
+        boxDisplay.comments + "<br><br>" + "JSON:<br>" + formData;
 };
 
 
 
 //Function to Validate Contact Form Entries
-function validate (evt){
+function validate (){
     let isValid = true;
 
     //validate first name entry
-    const firstName = $("#first_name").val().trim();
+    firstName = $("#first_name").val().trim();
     if (firstName =="") {
         $("#first_name").next().text("This field is required.");
         isValid = false;
@@ -27,7 +47,7 @@ function validate (evt){
 
 
     //validate last name entry
-    const lastName = $("#last_name").val().trim();
+    lastName = $("#last_name").val().trim();
     if (lastName == "") {
         $("#last_name").next().text("This field is required");
         isValid = false;
@@ -40,7 +60,7 @@ function validate (evt){
 
     //validate email address
     const emailPattern = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b/;
-    const email = $("#email_1").val().trim();
+    email = $("#email_1").val().trim();
     if (email == "") {
         $("#email_1").next().text("This field is required.");
         isValid = false;
@@ -56,7 +76,7 @@ function validate (evt){
 
 
     //validate comments section
-    const comments = $("#comments").val().trim();
+    comments = $("#comments").val().trim();
     if (comments == "") {
         $("label[for='comments']").next().text("\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0\xa0" + "Please enter a comment (required)");
         isValid = false;
@@ -66,22 +86,74 @@ function validate (evt){
     }
     $("#comments").val(comments);
 
-    //prevent default action if any entries are invalid
-    if (isValid == true) {
-        harvest();
-    }else{
-        evt.preventDefault();
+
+    //validate non-required fields
+    company = $("#company").val().trim();
+    if (company == "") {
+        company = "N/A";
     }
+    $("#company").val(company);
+
+    phone = $("#phone").val().trim();
+    if (phone == ""){
+        phone = "No Entry";
+    }
+    $("#phone").val(phone);
+
+
+    //Open dialog box if all entries are valid
+    if (isValid == true) {
+        $("#dialog").dialog("open");
+        harvest();
+    };
 };
 
 
 
 $(document).ready( () => {
 
+    // Initialize jQuery Widgets (Dialog Box)
+    $("#dialog").dialog({           
+        autoOpen: false,                
+        width: 465,
+        hide: { effect: "fadeOut", duration: 1000},
+        show: ("highlight",{color:"#ffffff"}, 1000),
+        close: function() {                         // You may have to write a function, here,
+            fadeOut.play();                         // to handle the clicks of two "confirm"
+            $("#opener").attr("disabled", false);   // and "edit" buttons.
+        },
+        open: function()  {
+            $("#submit").hide();
+            $("#reset").hide()
+        },
+        buttons: [
+            {
+                id: "confirm",
+                text: "Confirm",
+                click: function() {
+                    $(this).dialog("close");
+                    confirmed();
+                }
+            },
+            {
+                id: "edit",
+                text: "Edit",
+                click: function() {
+                    $(this).dialog("close");
+                    editForm();
+                }
+            }
+        ]          
+    
+    });
+
     // handle click on Form Submit button
     $("#submit").click( evt => {
-        validate(evt);
+        evt.preventDefault();
+        validate();
     });
+    // handle click on Form Reset button
+
 
 }); //end doc ready
 
